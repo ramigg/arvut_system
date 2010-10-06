c2b902be6e02f04daf429254564f9becdcefc9c4
Gregory Shilin <gshilin@gmail.com>
2010-10-06 19:58:48 +0200

    looking for ImageMagick executables

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,12 @@
 require "paperclip"
 
-Paperclip.options[:command_path] = 'c:/Program Files/ImageMagick-6.6.1-Q16'
+if File.exist?('c:/Program Files/ImageMagick-6.6.1-Q16/identify')
+  Paperclip.options[:command_path] = 'c:/Program Files/ImageMagick-6.6.1-Q16'
+elsif File.exist?('/opt/local/bin/identify')
+  Paperclip.options[:command_path] = '/opt/local/bin/'
+elsif File.exist?('/usr/bin/identify')
+  Paperclip.options[:command_path] = '/usr/bin/'
+else
+  raise 'Unable to find ImageMagic path'
+end
 Paperclip.options[:swallow_stderr] = false
