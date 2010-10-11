69d9481abdd2a140b8ab147170c7eab790f56246
Gregory Shilin <gshilin@gmail.com>
2010-10-11 17:20:58 +0200

    css

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,8 @@ class PagesController < ApplicationController
     @questionnaire_answer = QuestionnaireAnswer.new
     @questionnaire_answer.author = current_user
     @questionnaire_answer.answers = QuestionnaireAnswer.prepare_answers(@page)
+
+    @profile = current_user
   end
 
 end
