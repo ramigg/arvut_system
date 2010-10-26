0913fd8e41fd013aa1d668df244204be0b2b3e2b
Gregory Shilin <gshilin@gmail.com>
2010-10-26 20:16:10 +0200

    feed widget cache

--- app/cells/blog_widget.rb
+++ app/cells/blog_widget.rb
@@ -1,8 +1,9 @@
 class BlogWidget < Apotomo::Widget
 
+  cache :display, :expires_in => 20.minutes
+
   def display
     @feed = FeedReader::Basic.new(I18n.t('home.views.feed')).feed
-    render
+    render if @feed
   end
-
 end
