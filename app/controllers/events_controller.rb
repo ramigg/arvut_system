5cf3b2122e8bbcd4bab881a3f3147d15f8091ddc
Rami Grossman <ramigg@gmail.com>
2010-10-29 11:18:51 +0200

    events controller refactoring

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,10 @@
 class EventsController < ApplicationController
   layout 'stream'
+  respond_to :html, :js
 
-  def index
+  def show
+    @page = Page.find(params[:id])
+    respond_with @page
   end
 
 end
