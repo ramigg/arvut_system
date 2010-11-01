ab581be99048f71808b72bfddb4b4ffd974b2205
Rami Grossman <ramigg@gmail.com>
2010-11-01 11:40:29 +0200

    Can create events Events refactoring - move to apotomo - Forgotten files

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,13 +2,18 @@ class EventsController < ApplicationController
   layout 'stream'
   respond_to :html, :js
   
+    has_widgets do |event|
+      event << widget('stream_widget/admin', 'stream_admin', :display_form)
+      event << widget('stream_widget/container', 'stream_container', :display, :current_user => current_user)
+    end
   def show
     @page = Page.find(params[:id])
     
     
-    use_widgets do |event|
-      event << widget('stream_widget/admin', 'stream_admin', :display_form, :stream_preset => @page.stream_preset)
-    end
+    # use_widgets do |event|
+    #   event << widget('stream_widget/admin', 'stream_admin', :display_form)
+    # end
+    
     
     respond_with @page
   end
