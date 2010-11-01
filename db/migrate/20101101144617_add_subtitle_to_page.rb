688c1430f2975399ef16d25ec680828030b96a1f
Rami Grossman <ramigg@gmail.com>
2010-11-01 17:28:52 +0200

    Added subtitle field to pages - forgotten file

---
+++ db/migrate/20101101144617_add_subtitle_to_page.rb
@@ -0,0 +1,9 @@
+class AddSubtitleToPage < ActiveRecord::Migration
+  def self.up
+    add_column :pages, :subtitle, :string
+  end
+
+  def self.down
+    remove_column :pages, :subtitle
+  end
+end
