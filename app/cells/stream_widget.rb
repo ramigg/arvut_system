78c957a0e0f546f5cbf067a2e6165a7e76b97265
Gregory Shilin <gshilin@gmail.com>
2010-11-03 11:04:07 +0200

    Receive questions from kabtv Send question to kabtv

--- app/cells/stream_widget.rb
+++ app/cells/stream_widget.rb
@@ -34,56 +34,66 @@ module StreamWidget
     include ActionView::Helpers::JavaScriptHelper
     
     responds_to_event :more_questions, :with => :process_request
-    responds_to_event :submit, :with => :process_submit
+    responds_to_event :submit_question, :with => :process_submit
     
     def display
-      @questions = KabtvQuestion.approved_questions
+      @ask = KabtvQuestion.new
       render
     end
 
     def process_request
       last_question_id = (param :last_question_id).to_i
 
-      @questions = KabtvQuestion.approved_questions(last_question_id)
+      (@questions, @total_questions) = KabtvQuestion.approved_questions(last_question_id)
 
       if @questions.empty?
         if last_question_id == 0
           # no questions => no questions
           render :text => '', :content_type => Mime::JS
-        else
-          # questions => no questions
+        elsif @total_questions == 0
+          # questions => no new questions
           render :text => "
             $('dl#questions').html('#{escape_javascript '<dd class="even">No questions yet.</dd>'.html_safe}');
             kabtv.questions.last_question_id = 0;
           ", :content_type => Mime::JS
+        else
+          render :text => '', :content_type => Mime::JS
         end
       else
         content = ''
         @questions.each_with_index {|q, idx|
           klass = ((last_question_id + 1 + idx) & 1) == 0 ? 'odd' : 'even'
+          name = q.qname.empty? ? 'Guest' : q.qname
+          from = q.qfrom.empty? ? 'Somewhere' : q.qfrom
+          style = q.lang == 'Hebrew' ? 'style="direction:rtl"' : ''
           content += <<-HTML
-            <dt class="#{klass}"><span class="who">#{q.qname}</span>, <span class="where">#{q.qfrom}</span></dt>
-            <dd class="#{klass}">#{q.qquestion}</dd>
+            <dt class="#{klass}" #{style}><span class="who">#{name}</span> @ <span class="where">#{from}</span></dt>
+            <dd class="#{klass}" #{style}>#{q.qquestion}</dd>
           HTML
         }
         if last_question_id == 0
           # no questions => questions
           render :text => "
             $('dl#questions').html('#{escape_javascript content.html_safe}');
-            kabtv.questions.last_question_id = #{@questions.count};
+            kabtv.questions.last_question_id = #{@questions.last.id};
           ", :content_type => Mime::JS
         else
           # questions => more questions
           render :text => "
             $('dl#questions').append('#{escape_javascript content.html_safe}');
-            kabtv.questions.last_question_id =+ #{@questions.count};
+            kabtv.questions.last_question_id =+ #{@questions.last.id};
           ", :content_type => Mime::JS
         end
       end
     end
 
     def process_submit
-      
+      if KabtvQuestion.ask_question(param :kabtv_question)
+        message = 'Your question is awaiting approval'
+      else
+        message = 'Please fill "Question" field in'
+      end
+      render :text => "alert('#{message}');", :content_type => Mime::JS
     end
   end
 
@@ -111,7 +121,8 @@ module StreamWidget
           images << "<img alt='' src='#{url}/#{img}'></img>"
         }
       end
-      render :text => "$('.images').append('#{escape_javascript images.join('').html_safe}');", :content_type => Mime::JS
+      text = images.empty? ? '' : "$('.images').append('#{escape_javascript images.join('').html_safe}');"
+      render :text => text, :content_type => Mime::JS
     end
   end
 
--- app/models/kabtv_question.rb
+++ app/models/kabtv_question.rb
@@ -3,12 +3,45 @@ class KabtvQuestion < Kabtv
   
   def self.approved_questions(last_question_id = 0)
     sql = <<-SQL
-      SELECT qfrom, qname, qquestion, who
+      SELECT id, qfrom, qname, qquestion, who, lang
         FROM questions WHERE
          (id > #{last_question_id}) AND
          (isquestion = 1) AND (is_hidden=0 OR is_hidden IS NULL) AND (approved <> 0)
         ORDER BY id ASC
     SQL
-    find_by_sql(sql)
+    new_questions = find_by_sql(sql)
+    sql = <<-SQL
+      SELECT count(*)
+        FROM questions WHERE
+         (isquestion = 1) AND (is_hidden=0 OR is_hidden IS NULL) AND (approved <> 0)
+    SQL
+    total_questions = find_by_sql(sql)
+
+    [new_questions, total_questions]
+  end
+
+  def self.ask_question(question)
+    return nil if question[:qquestion].empty?
+    
+    question[:lang] = case I18n.locale
+    when :en
+      'English'
+    when :he
+      'Hebrew'
+    when :ru
+      'Russian'
+    when :de
+      'German'
+    when :it
+      'Italian'
+    when :es
+      'Spanish'
+    when :fr
+      'French'
+    end
+    question[:isquestion] = 1
+    question[:is_hidden] = 0
+    question[:timestamp] = Time.now.to_s(:db)
+    KabtvQuestion.new(question).save(false)
   end
 end
