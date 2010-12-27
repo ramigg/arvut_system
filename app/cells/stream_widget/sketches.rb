eef4b6072ddd51b8c9038dc4888b7772ad1c819b
Gregory Shilin <gshilin@gmail.com>
2010-12-27 13:55:46 +0200

    merged with Rami's changes

--- app/cells/stream_widget/sketches.rb
+++ app/cells/stream_widget/sketches.rb
@@ -1,7 +1,7 @@
 module StreamWidget
   class Sketches < Apotomo::Widget
     responds_to_event :classboard, :with => :display_classboard
-    
+
     def display
       return unless current_preset.show_sketches
 
@@ -10,30 +10,35 @@ module StreamWidget
 
     def display_classboard
       images = []
+      reset = false
       begin
         data = EventDataReader::ClassBoard.new.classboard
         sketches_url = data[:urls][:sketches]
         sketches = data[:thumbnails]
         last_one = params[:total].to_i
         total = sketches.size
-        last_one = 0 if last_one > total
-        sketches = sketches[last_one .. total] || []
+        reset = last_one > total
+        sketches = reset ? sketches : (sketches[last_one .. total] || [])
         images = sketches.map{ |img|
           "<img alt='' src='#{sketches_url}/#{img}'></img>"
         }
       end
-      text = if images.empty?
+      text = if images.empty? && !reset
+        # Nothing to show (and it was not reset)
         total == 0 ?
-          '$(\'.images\').html(\'\')' :
-          ''
+          # No sketches on server
+        '$(\'.images\').html(\'\')' :
+          # No new sketches
+        ''
       else
-        total > last_one && last_one != 0 ?
+        reset ?
+          # Less files... Or reset had happened or some images were removed
+        "$('.images').html('#{escape_javascript images.join('').html_safe}');" :
           # New files were added
-          "$('.images').append('#{escape_javascript images.join('').html_safe}');" :
-          # Less files... Supposedly reset happened
-          "$('.images').html('#{escape_javascript images.join('').html_safe}');"
+        "$('.images').append('#{escape_javascript images.join('').html_safe}');"
       end
       render :text => text, :content_type => Mime::JS
     end
   end
 end
+
