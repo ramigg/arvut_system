11b956efe63fcc8d9d6eeb0660071c3b3ddc1c38
gshilin <gshilin@gmail.com>
2011-03-06 18:02:56 +0200

    WSUP-972 пустые комментарии на свиве

--- app/cells/pages_admin.rb
+++ app/cells/pages_admin.rb
@@ -9,32 +9,38 @@ class PagesAdmin < Apotomo::Widget
     @lang_id = Language.get_id_by_locale(params[:language] || I18n.locale)
     @parent_id = param(:parent_id)
   end
+
   private(:prepare_params)
 
   def display_form
     page_type = 'message'
     @page = Page.new(:language_id => @lang_id, :author_id => current_user.id,
-      :status => 'PUBLISHED', :page_type => page_type)
+                     :status => 'PUBLISHED', :page_type => page_type)
     render
   end
 
   def display_comments_form
     return unless @parent_id
     @page_comment = Page.new(:language_id => @lang_id, :author_id => current_user.id,
-      :status => 'PUBLISHED', :page_type => 'message',
-      :parent_id => @parent_id
+                             :status => 'PUBLISHED', :page_type => 'message',
+                             :parent_id => @parent_id
     )
     render
   end
-  
+
   def process_form
-    @page = Page.new(params[:page])
-    @page.assets.sort! { |a, b| a.position <=> b.position } if @page.assets
-    @page.publish_at = Time.zone.now
-    @success = @page.save :validate => false
+    if params[:page][:message_body].empty?
+      @success = false
+      @specific_message = 'Please write something'
+    else
+      @page = Page.new(params[:page])
+      @page.assets.sort! { |a, b| a.position <=> b.position } if @page.assets
+      @page.publish_at = Time.zone.now
+      @success = @page.save :validate => false
+    end
     render
   end
-  
+
   def ckeditor_toolbar(klass = 'Min')
     is_rtl? ? "#{klass}_he" : klass
   end
