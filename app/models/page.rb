5bc7080473bc30f89568b77320e8e86a11d7682f
gshilin <gshilin@gmail.com>
2011-03-22 12:50:01 +0200

    WSUP-986 - bug in search on sviva tova

--- app/models/page.rb
+++ app/models/page.rb
@@ -15,7 +15,7 @@ class Page < ActiveRecord::Base
   accepts_nested_attributes_for :assets, :allow_destroy => true
   accepts_nested_attributes_for :taggings, :allow_destroy => true
 
-  [:article_resources, :video_resources, :audio_resources, :questions, :intention_resources].each{|resource|
+  [:article_resources, :video_resources, :audio_resources, :questions, :intention_resources].each { |resource|
     has_many resource, :through => :assets
   }
 
@@ -28,7 +28,7 @@ class Page < ActiveRecord::Base
   #  *Validations*
 
   validates :title, :presence => true
-  validates :message_body, :presence => true, :if => lambda{|e| e.page_type == 'message'}
+  validates :message_body, :presence => true, :if => lambda { |e| e.page_type == 'message' }
 
   delegate :locale, :to => :language
 
@@ -44,16 +44,16 @@ class Page < ActiveRecord::Base
 
   #  *Scopes*
 
-  scope :published, lambda {where(:publish_at.lt => Time.zone.now, :status => 'PUBLISHED')}
+  scope :published, lambda { where(:publish_at.lt => Time.zone.now, :status => 'PUBLISHED') }
   scope :no_parent, where(:parent_id => nil)
 
   #  scope :by_conf_date, lambda {|user_reg_date| where(:publish_at.gt => user_reg_date)}
 
-  scope :by_page_type, lambda {|page_type, language_id, user_reg_date|
+  scope :by_page_type, lambda { |page_type, language_id, user_reg_date|
     (page_type == 'all' || page_type == 'tag' ?
         where(:language_id => language_id) :
         where(:language_id => language_id, :page_type => page_type.singularize)).
-      no_parent.published
+        no_parent.published
   }
 
   #  scope :all_pages, lambda {|language_id| where(:language_id => language_id)}
@@ -64,25 +64,25 @@ class Page < ActiveRecord::Base
   #   joins(:page_userflags).where(:page_userflags => {:user_id => user_id, :is_read => true}) 
   # }
 
-  scope :comments, lambda {|parent_id, language_id|
+  scope :comments, lambda { |parent_id, language_id|
     where(:language_id => language_id, :page_type => 'message', :parent_id => parent_id).
-      published.ordered
+        published.ordered
   }
 
-  scope :completed_assignments, lambda {|language_id, user_reg_date, user_id|
+  scope :completed_assignments, lambda { |language_id, user_reg_date, user_id|
     by_page_type('assignment', language_id, user_reg_date).joins(:page_userflags).where(:page_userflags => {:user_id => user_id, :is_answered => true})
   }
 
-  scope :favorite_pages, lambda {|language_id, user_reg_date, user_id|
+  scope :favorite_pages, lambda { |language_id, user_reg_date, user_id|
     by_page_type('all', language_id, user_reg_date).joins(:page_userflags).where(:page_userflags => {:user_id => user_id, :is_bookmark => true})
   }
 
-  scope :read_pages_by_page_type, lambda {|page_type, language_id, user_reg_date, user_id|
+  scope :read_pages_by_page_type, lambda { |page_type, language_id, user_reg_date, user_id|
     by_page_type(page_type, language_id, user_reg_date).joins(:page_userflags).where(:page_userflags => {:user_id => user_id, :is_read => true})
   }
 
   # .where(:id - PageUserflag.select(:id).where({:user_id => u.id} & {:is_read => true}))
-  scope :new_pages_by_page_type, lambda {|page_type, language_id, user_reg_date, user_id|
+  scope :new_pages_by_page_type, lambda { |page_type, language_id, user_reg_date, user_id|
     by_page_type(page_type, language_id, user_reg_date).where("pages.id not in (select puf.page_id from page_userflags as puf where puf.user_id = #{user_id} and puf.is_read = true)")
   }
 
@@ -91,10 +91,14 @@ class Page < ActiveRecord::Base
   # end
 
   def self.search(search)
-    if search
-      search = "%#{search}%"
-      where((:title.matches % search) | (:description.matches % search) |
-        (:message_body.matches % search)  | (:subtitle.matches % search))
+    unless search.empty?
+      search = "%#{search.strip}%"
+      joins(:article_resources).
+          where(
+          (:title.matches % search) | (:description.matches % search) |
+              (:message_body.matches % search) | (:subtitle.matches % search) |
+              {:article_resources => [:body.matches % search]}
+      )
     else
       scoped
     end
@@ -105,11 +109,11 @@ class Page < ActiveRecord::Base
 
     if filter[:status] && filter[:status] != ''
       page = case filter[:status]
-      when 'FUTURE'
-        page.where(:status => 'PUBLISHED', :publish_at.gt => Time.zone.now)
-      else
-        page.where(:status => filter[:status])
-      end
+               when 'FUTURE'
+                 page.where(:status => 'PUBLISHED', :publish_at.gt => Time.zone.now)
+               else
+                 page.where(:status => filter[:status])
+             end
     end
     page = page.where(:page_type => filter[:page_type]) if filter[:page_type] && filter[:page_type] != ''
     page = page.where(:author_id => filter[:author]) if filter[:author] && filter[:author] != ''
@@ -140,13 +144,15 @@ class Page < ActiveRecord::Base
   def has_questions?
     questions
   end
+
   def self.all_tags_strings(locale)
-    tag_counts_on(:"#{locale}_tags").all.map{|e| e.name}
+    tag_counts_on(:"#{locale}_tags").all.map { |e| e.name }
   end
+
   def self.all_tags(locale)
     result = tag_counts_on(:"#{locale}_tags").all
     if result && result.size > 0
-      result = result.select{|r| Page.published.tagged_with(r.name).count > 0}
+      result = result.select { |r| Page.published.tagged_with(r.name).count > 0 }
       result.empty? ? nil : result
     else
       nil
@@ -161,10 +167,12 @@ class Page < ActiveRecord::Base
     flags = page_userflags.where(:user_id => user.id).first rescue nil
     flags ? flags.is_read? : false
   end
+
   def is_bookmarked?(user)
     flags = page_userflags.where(:user_id => user.id).first rescue nil
     flags ? flags.is_bookmark? : false
   end
+
   def is_answered?(user)
     flags = page_userflags.where(:user_id => user.id).first rescue nil
     flags ? flags.is_answered? : false
