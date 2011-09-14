2a219886f49c3c2465a3e46074ebcfc69c447ca5
Kolman <kolmanv@gmail.com>
2011-09-14 20:38:54 +0300

    small changes.

--- public/javascripts/stream_widget/stream_comet_update.js
+++ public/javascripts/stream_widget/stream_comet_update.js
@@ -62,14 +62,14 @@ function publish_stream_preset_4_comet() {
         success: publish_data_to_comet
     });
 
-     function publish_data_to_comet_label(data) {
+    function publish_data_to_comet_label(data) {
         if (typeof(stream_comet_update_app) != "undefined"
             && stream_comet_update_app != null) {
             stream_comet_update_app.update_others_label(data);
         }
     }
 
-     kabtv.tabs.poll_support && $.ajax({
+    kabtv.tabs.poll_support && $.ajax({
         url: 'http://live.kab.tv/button.php',
         data: {
             image: 'tech',
@@ -86,7 +86,7 @@ function delayedUpdataAppInit() {
     if (comet_app.isConnected()) {
         stream_comet_update_app = new $.SteamCometUpdateApp(comet_app);
     } else {
-        setTimeout(delayedUpdataAppInit, 1000)
+        setTimeout(delayedUpdataAppInit, 100)
     }
 }
 
