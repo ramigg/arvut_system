77acf31c736b73bb14b6291dd701a2940c42c382
Gregory Shilin <gshilin@gmail.com>
2012-01-15 11:03:24 +0200

    cache tags according to locale

--- app/cells/tags_widget.rb
+++ app/cells/tags_widget.rb
@@ -1,6 +1,6 @@
 class TagsWidget < Apotomo::Widget
 
-  cache :display, :expires_in => 120.minutes
+  cache :display, Proc.new { {:locale => I18n.locale} }, :expires_in => 120.minutes
 
   def display
     @tags = Page.all_tags(I18n.locale)
