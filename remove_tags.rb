ce7186653261082f3fb2069f8d0a1bbc382033f0
Gregory Shilin <gshilin@gmail.com>
2014-03-02 11:50:44 +0200

    added tags and taggings + script to change/remove tags

---
+++ remove_tags.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+tags2remove = [
+]
+
+tags2remove.each do |tag_name|
+  tag = Tag.where(name: tag_name).first
+  unless tag
+    puts "Not found tag %#{tag_name}%"
+    next
+  end
+  taggings = tag.taggings
+  puts "Found #{taggings.count} taggings for tag #{tag.name}"
+  taggings.each {|t| t.destroy}
+  tag.destroy
+end
+
+tag = Tag.where(name: 'עשיריות').first
+tag.name = 'חמשיות' 
+tag.save
+
