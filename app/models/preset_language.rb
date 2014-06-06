c987ee2a33459e8876350779156fe3dcba311e4d
Gregory Shilin <gshilin@gmail.com>
2014-06-06 14:02:37 +0300

    workaround to create a new language

--- app/models/preset_language.rb
+++ app/models/preset_language.rb
@@ -7,3 +7,5 @@ class PresetLanguage < ActiveRecord::Base
   has_many :stream_items
   accepts_nested_attributes_for :stream_items, :allow_destroy => true
 end
+
+# PresetLanguage.where(stream_preset_id: 3 (Special Lesson), language_id: Languange.find(xxx), technology_id: 3(Flash)).first.update_attribute(:quality_id, 1)
