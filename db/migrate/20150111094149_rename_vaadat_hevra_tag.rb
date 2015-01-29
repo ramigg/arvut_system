6a5a02115adc3a9b532646881cf20bb3070ecfdf
userruby <bbruby1sv@gmail.com>
2015-01-29 08:31:57 +0000

    misc

--- db/migrate/20150111094149_rename_vaadat_hevra_tag.rb
+++ db/migrate/20150111094149_rename_vaadat_hevra_tag.rb
@@ -1,11 +1,12 @@
+# encoding: utf-8
 class RenameVaadatHevraTag < ActiveRecord::Migration
   def self.up
-    t = Tag.where(name: 'ועדת חברה')
+    t = Tag.where(name: 'ועדת חברה').first
     t.update_attribute(:name, 'צוות חברתי')
   end
 
   def self.down
-    t = Tag.where(name: 'צוות חברתי')
+    t = Tag.where(name: 'צוות חברתי').first
     t.update_attribute(:name, 'ועדת חברה')
   end
 end
