9ed712e061d97ceeeb7469db428efcef381c1293
ramigg <ramigg@gmail.com>
2015-08-06 15:15:53 +0300

    fixed authentication api

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,3 +1,8 @@
 class Role < ActiveRecord::Base
   has_and_belongs_to_many :users
+
+  def self.archived_broadcasts
+    @@archived_broadcasts ||= where(role: 'archived_broadcasts').first
+  end
+
 end
